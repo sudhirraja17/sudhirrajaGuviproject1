29e2b1d75541a581c77853a1036b26e94f4ffec2
sudhirraja17 <84018607+sudhirraja17@users.noreply.github.com>
2022-07-25 16:05:16 +0530

    Update pytest_sudhir.py

--- pytest_sudhir.py
+++ pytest_sudhir.py
@@ -1,14 +1,50 @@
 import main
 import pytest
+import pandas as pd
+import time
 
 g = main.Guvi()
 
-# test for login to guvi portal
+# test for login to zen portal
 @pytest.mark.first
 def test_login():
     g.login()
+    time.sleep(5)
     assert g.driver.current_url == "https://www.zenclass.in/class"
 
+# test for information getting in left ribbon
 @pytest.mark.second
+def test_main_menu():
+    """ access items from the menubar"""
+    l_menu = main.g.driver.execute_script(
+        " l_menu_items=document.getElementsByClassName"
+        '("list-scroll py-3 color-area");'
+        " l_menu_logo= document.getElementsByClassName"
+        '("ml-3 d-inline-block mt-3 font-weight-bold")[0].innerText;'
+        "l_menu_head=document.getElementsByClassName"
+        '("list-scroll py-3 active-area active-left-bar")[0].innerText;'
+        " l_menu=[];"
+        """for (let index = 0; index < l_menu_items.length; index++)
+               {
+               l_menu[index]=l_menu_items.item(index).innerText;
+               }
+               l_menu.splice(0,0,l_menu_logo,l_menu_head);
+               return l_menu ;"""
+    )
+    time.sleep(5)
+    df_fm = pd.DataFrame(l_menu, columns=["Main-menu"])
+    # check dataframes
+    pd.testing.assert_frame_equal(main.g.main_menu(), df_fm)
+    time.sleep(5)
+
+# test for raise a query in zen portal
+@pytest.mark.thrid
 def test_createquery():
-    g.queue()
+    """ check the header of the newly created query in the query page"""
+    query_text_head = "Guvi Python AT - 1 &2 Automation Project"
+    assert query_text_head in main.g.query
+    assert query_text_head in main.g.query
+    assert query_text_head in main.g.query
+    assert query_text_head in main.g.query
+    assert query_text_head in main.g.query
+
